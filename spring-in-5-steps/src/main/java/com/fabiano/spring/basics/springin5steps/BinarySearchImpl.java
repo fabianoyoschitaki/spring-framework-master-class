54f1e24d86449984636ae6d61dc46f31b1eda37d
Fabiano <fabianoyoschitaki@gmail.com>
2018-09-17 20:51:07 -0300

    ok

--- spring-in-5-steps/src/main/java/com/fabiano/spring/basics/springin5steps/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/fabiano/spring/basics/springin5steps/BinarySearchImpl.java
@@ -9,11 +9,17 @@ public class BinarySearchImpl {
 	@Autowired
 	private SortAlgorithm sortAlgorithm;
 	
+	//Constructor Injection
 	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
 		super();
 		this.sortAlgorithm = sortAlgorithm;
 	}
 	
+	//Setter Injection
+	public void setSortAlgorithm(SortAlgorithm sortAlgorithm) {
+		this.sortAlgorithm = sortAlgorithm;
+	}
+
 	public int binarySearch(int [] numbers, int numberToSearchFor) {
 		int sortedNumbers[] = sortAlgorithm.sort(numbers);
 		System.out.println(sortAlgorithm);

e0db68af969745ffce53487ce4a742259f6dcb13
Fabiano <fabianoyoschitaki@gmail.com>
2018-09-19 22:51:08 -0300

    ok

--- spring-in-5-steps/src/main/java/com/fabiano/spring/basics/componentscan/ComponentJdbcConnection.java
+++ spring-in-5-steps/src/main/java/com/fabiano/spring/basics/componentscan/ComponentJdbcConnection.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
-//only prototype does not assure it will give different bean each time. create a proxy
 @Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode=ScopedProxyMode.TARGET_CLASS)
 public class ComponentJdbcConnection {
 	public ComponentJdbcConnection() {
--- spring-in-5-steps/src/main/java/com/fabiano/spring/basics/springin5steps/SpringIn5StepsComponentScanApplication.java
+++ spring-in-5-steps/src/main/java/com/fabiano/spring/basics/springin5steps/SpringIn5StepsComponentScanApplication.java
@@ -1,25 +1,24 @@
 package com.fabiano.spring.basics.springin5steps;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-import com.fabiano.spring.basics.springin5steps.basic.BinarySearchImpl;
+import com.fabiano.spring.basics.componentscan.ComponentDAO;
 
 @SpringBootApplication
+@ComponentScan("com.fabiano.spring.basics.componentscan") 
 public class SpringIn5StepsComponentScanApplication {
-	
-	//What are the beans?
-	//What are the dependencies of a bean?
-	//Where to search for beans? => No need as SpringBootApplication scans package and subs
+
+	static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsComponentScanApplication.class);
 	
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsComponentScanApplication.class, args);
 		
-		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
-		BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
-		System.out.println(binarySearch.equals(binarySearch1));// same bean as default scope is singleton
-		
-		System.out.println(binarySearch.binarySearch(new int[] {1,2,3}, 3));
+		ComponentDAO componentDao = applicationContext.getBean(ComponentDAO.class);
+		LOGGER.info("{}", componentDao);
 	}
 }

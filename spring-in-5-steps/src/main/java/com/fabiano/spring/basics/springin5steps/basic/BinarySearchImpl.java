8a2e657ae06c9bfed8f5580c0e715620cc8bacd6
Fabiano <fabianoyoschitaki@gmail.com>
2018-09-19 23:03:35 -0300

    predestroy, postconstruct

--- spring-in-5-steps/src/main/java/com/fabiano/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/fabiano/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,5 +1,10 @@
 package com.fabiano.spring.basics.springin5steps.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
@@ -8,6 +13,8 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)// creates new bean every time
 public class BinarySearchImpl {
+	
+	static Logger LOGGER = LoggerFactory.getLogger(BinarySearchImpl.class);
 	
 	@Autowired
 	private SortAlgorithm sortAlgorithm;
@@ -27,5 +34,15 @@ public class BinarySearchImpl {
 		int sortedNumbers[] = sortAlgorithm.sort(numbers);
 		System.out.println(sortAlgorithm);
 		return 3;
+	}
+	
+	@PostConstruct
+	public void postConstruct() {
+		LOGGER.info("postConstruct is called.");
+	}
+	
+	@PreDestroy
+	public void preDestroyMethod() {
+		LOGGER.info("preDestroyMethod is called.");
 	}
 }

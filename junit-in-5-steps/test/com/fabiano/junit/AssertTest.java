48b6659dc280f446982b5d7d01200bdafa8e3a91
Fabiano Shoji Yoschitaki <fabianoyoschitaki@gmail.com>
2018-11-25 01:39:14 -0200

    ok

---
+++ junit-in-5-steps/test/com/fabiano/junit/AssertTest.java
@@ -0,0 +1,20 @@
+package com.fabiano.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AssertTest {
+
+	@Test
+	public void test() {
+		boolean condition = true;
+		assertEquals(2, 2);
+		assertTrue(condition);
+		assertFalse(!condition);
+		assertNotNull(new Object());
+		assertNull(null);
+		assertArrayEquals(new int[] {1,2,3}, new int[] {1,2,3});
+	}
+
+}

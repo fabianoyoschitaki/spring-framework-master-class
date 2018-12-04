fac53255ec4e8b76bd5f454251f3e80ed259d2ab
Fabiano <fabianoyoschitaki@gmail.com>
2018-12-03 23:49:46 -0200

    mockito

--- junit-in-5-steps/test/com/fabiano/junit/AssertTest.java
+++ junit-in-5-steps/test/com/fabiano/junit/AssertTest.java
@@ -1,8 +1,12 @@
 package com.fabiano.junit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class AssertTest {
---
+++ mockito-in-5-steps/src/main/java/com/fabiano/mockito/mockitoin5steps/MockitoIn5StepsApplication.java
@@ -0,0 +1,12 @@
+package com.fabiano.mockito.mockitoin5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoIn5StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoIn5StepsApplication.class, args);
+	}
+}

067ac9d1af4e2cfb49da6bd9f068eece5b56757e
Fabiano <fabianoyoschitaki@gmail.com>
2018-12-04 00:09:43 -0200

    mokcito

---
+++ mockito-in-5-steps/src/main/java/com/fabiano/mockito/mockitoin5steps/DataService.java
@@ -0,0 +1,5 @@
+package com.fabiano.mockito.mockitoin5steps;
+
+public interface DataService {
+	int[] retrieveAllData();
+}
---
+++ mockito-in-5-steps/src/main/java/com/fabiano/mockito/mockitoin5steps/SomeBusinessImpl.java
@@ -0,0 +1,22 @@
+package com.fabiano.mockito.mockitoin5steps;
+
+//SomeBusinessImpl is dependent on DataService.
+public class SomeBusinessImpl {
+	private DataService dataService;
+	
+	public SomeBusinessImpl(DataService dataService) {
+		super();
+		this.dataService = dataService;
+	}
+
+	int findTheGreatestFromAllData() {
+		int[] data = dataService.retrieveAllData();
+		int greatest = Integer.MIN_VALUE;
+		for (int value : data) {
+			if (value > greatest) {
+				greatest = value;
+			}
+		}
+		return greatest;
+	}
+}

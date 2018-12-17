bfe9cd56f955de324ee5f9c9cbb40be1a5cda2c1
Fabiano <fabianoyoschitaki@gmail.com>
2018-12-17 14:36:50 -0200

    first rest controller

--- springboot-in-10-steps/src/main/java/com/fabiano/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ springboot-in-10-steps/src/main/java/com/fabiano/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootIn10StepsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootIn10StepsApplication.class, args);
 	}
-
 }
 
